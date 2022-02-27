389d9d48bf0da71c96061b61d301e09afecff7ff
Rakhilca Yanedika <90024215+ryanedika@users.noreply.github.com>
2022-02-27 15:02:16 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -2,7 +2,7 @@ public class Nomor2 {
     public static void main(String[] args) {
         String[] nama= {"D","I","K","A"};
 
-        System.out.println("The size of the array is" + nama.length);
+        System.out.println("The size of the array is = " + nama.length);
     
 }
     
